07bed6ea59303977062a333f77d312b3e5795d3e
luijp <luijunpeng@outlook.com>
2023-12-11 12:04:01 +0800

    init project

---
+++ src/main/java/cn/luijp/wikinn/WikinnApplication.java
@@ -0,0 +1,13 @@
+package cn.luijp.wikinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikinnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikinnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/luijp/wikinn/WikinnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.luijp.wikinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikinnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
